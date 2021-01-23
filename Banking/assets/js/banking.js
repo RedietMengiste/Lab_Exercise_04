adc2f59d1301f987f9bd5d5727b5e413c0e3ce82
RedietMengiste <75179876+RedietMengisteKebede@users.noreply.github.com>
2021-01-23 20:46:30 +0300

    modified the trasfer method return value and of two accounts.

--- Banking/assets/js/banking.js
+++ Banking/assets/js/banking.js
@@ -12,44 +12,50 @@ var Account_1 = {
             console.log("You cannot withdraw more than your balance, please check you balance and try again!");
         }
         else
-        console.log( "You have succesfully withdrawn " + withdrawAmount + " birr."); 
-        return this.Balance-=withdrawAmount;
+        // console.log( "You have succesfully withdrawn " + withdrawAmount + " birr."); 
+        this.Balance-=withdrawAmount;
+        return withdrawAmount;
     },
     check_balance: function(){
         var chooseAct = parseInt(prompt("Choose account 1('1') or account 2('2'):"))
         if (chooseAct==1){
-        console.log("Your account balance is: "+ this.Balance)}
+           return this.Balance
+    }
         if (chooseAct==2){
-            console.log("Your account balance is: "+ Account_2.Balance2)
+            return Account_2.Balance2
         }
     },
 
     transfer:function(){
+        
         //var Balance=10000;
         var chooseAct = parseInt(prompt("Choose account to whom you want to transfer 1('1') or account 2('2'):"))
         if (chooseAct==1){
             var Rname = prompt("Enter reciepients full name: ");
-        var transferedAmount = parseInt(prompt("Enter the amount of money you want to send to Account 1: "));
-    // 9900 is the max transfer amount
-        if (transferedAmount >= this.Balance || transferedAmount<100)
-            console.log("You cannot transfer more than your balance, please try again!");
+            var transferedAmount = parseInt(prompt("Enter the amount of money you want to send to Account 1: "));
+ 
+        if (transferedAmount >= this.Balance || transferedAmount <100)
+            console.log("You cannot transfer more than your balance and below 100br, please try again!");
+
         else 
-            
-            console.log( "You have succesfully transferred " + transferedAmount + " birr to "+ Rname); 
+           
             this.Balance += transferedAmount;
             Account_2.Balance2 -= transferedAmount;
+            return "You have succesfully transferred " + transferedAmount + "birr to " + Rname;
         }
+
         if (chooseAct==2){
             var Rname = prompt("Enter reciepients full name: ");
-        var transferedAmount = parseInt(prompt("Enter the amount of money you want to send to Account 2: "));
-    // 9900 is the max transfer amount
-        if (transferedAmount >= this.Balance || transferedAmount<100)
-            console.log("You cannot transfer more than your balance, please try again!");
+            var transferedAmount = parseInt(prompt("Enter the amount of money you want to send to Account 2: "));
+   
+        if (transferedAmount >= this.Balance || transferedAmount <100)
+            console.log("You cannot transfer more than your balance and below 100br, please try again!");
         else 
             
-            console.log( "You have succesfully transferred " + transferedAmount + " birr to "+ Rname); 
+           
             this.Balance -= transferedAmount;
             Account_2.Balance2 += transferedAmount;
+            return "You have succesfully transferred " +  transferedAmount + "birr to " + Rname;
         }
         
     },
@@ -70,14 +76,14 @@ let fun = (function(){
     
     switch(userPrompt){
         case 'b':
-            console.log(Account_1.check_balance());
+            console.log( "Your account balace is : " + Account_1.check_balance()+" birr.");
             break;
         case 'w':
-            console.log(Account_1.withdraw());
+            console.log("You have successfully Withdrawn " + Account_1.withdraw()+" birr.");
             break;
 
         case 'd':
-            console.log("You have succesfully deposited " + Account_1.deposit());
+            console.log("You have succesfsully deposited " + Account_1.deposit()+" birr.");
             break;
         case 't':
             console.log(Account_1.transfer());
@@ -91,93 +97,3 @@ let fun = (function(){
 }
 
 })();
-
-// var Balance = 10000;
-// var Balance2 = 4000;
-
-// function transfer(){
-//     //var Balance=10000;
-//     var chooseAct = parseInt(prompt("Choose account to whom you want to transfer 1('1') or account 2('2'):"))
-//     if (chooseAct==1){
-//         var Rname = prompt("Enter reciepients full name: ");
-//     var transferedAmount = parseInt(prompt("Enter the amount of money you want to send to Account 1: "));
-// // 9900 is the max transfer amount
-//     if (transferedAmount >= Balance || transferedAmount<100)
-//         console.log("You cannot transfer more than your balance, please try again!");
-//     else 
-        
-//         console.log( "You have succesfully transferred " + transferedAmount + " birr to "+ Rname); 
-//         Balance += transferedAmount;
-//         Balance2 -= transferedAmount;
-//     }
-//     if (chooseAct==2){
-//         var Rname = prompt("Enter reciepients full name: ");
-//     var transferedAmount = parseInt(prompt("Enter the amount of money you want to send to Account 2: "));
-// // 9900 is the max transfer amount
-//     if (transferedAmount >= Balance || transferedAmount<100)
-//         console.log("You cannot transfer more than your balance, please try again!");
-//     else 
-        
-//         console.log( "You have succesfully transferred " + transferedAmount + " birr to "+ Rname); 
-//         Balance -= transferedAmount;
-//         Balance2 += transferedAmount;
-//     }
-    
-// }
-
-
-// function withdraw(){
-//     var withdrawAmount = parseInt(prompt("Enter the amount of money you want to withdraw: "));
-//     if (Balance <= withdrawAmount){
-//         console.log("You cannot withdraw more than your balance, please check you balance and try again!");
-//     }
-//     else
-//     console.log( "You have succesfully withdrawn " + withdrawAmount + " birr."); 
-//     Balance-=withdrawAmount;
-// }
-
-
-// function deposit(){
-//     depositAmount = parseInt(prompt("How many values do you want to deposit?"));
-//     console.log( "You have succesfully deposited " + depositAmount + " birr."); 
-//     Balance+=depositAmount;
-    
-// }
-
-
-// function balance(){
-//     var chooseAct = parseInt(prompt("Choose account 1('1') or account 2('2'):"))
-//     if (chooseAct==1){
-//     console.log("Your account balance is: "+ Balance)}
-//     if (chooseAct==2){
-//         console.log("Your account balance is: "+ Balance2)
-//     }
-// }
-
-
-// (function(){
-//     var choice = parseInt(prompt("Welcome! please write '1' to continue: else enter '0'"))
-//  while (choice) {
-    
-   
-//     var userPrompt = prompt("Enter 'b' for balance /n Enter 'w' for Withdrawal /n Enter 'd' for deposit n/ Enter 't' for transfer ?");
-//     var choice = parseInt(prompt("Welcome! please write '1' to continue: else enter '0'"))
-//     switch(userPrompt){
-//         case 'b':
-//             console.log(balance());
-//             break;
-//         case 'w':
-//             console.log(withdraw());
-//             break;
-
-//         case 'd':
-//             console.log(deposit());
-//             break;
-//         case 't':
-//             console.log(transfer());
-//             break;
-//     }
-  
-// }
-
-// })();
